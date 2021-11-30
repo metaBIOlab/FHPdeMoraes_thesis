a71327fb347846df45d2441eecc451036b3b7e42
moraesfernanda <fernandahmoraes@gmail.com>
2021-11-30 18:00:58 -0300

    update and clean up

--- Complementaryanalysis2_predictivemodel/analises/reorg/Neuroquant.R
+++
@@ -1,3 +0,0 @@
-# NEUROQUANT
-
-dados_raw %>% select(SUBJ_ses, Session, Diagnostic, ESC, Lipoxina, NeuroQuant) %>% basic_eda()
--- Complementaryanalysis2_predictivemodel/cor_slope_age_interval.R
+++
@@ -1,9 +0,0 @@
-lm_Age <- filter(dados_hemi_v1, Diagnostic == "CTL", Age_interval != "40-45", Age_interval != "80-85") %>%
-  group_by(Age_interval) %>%
-  do(fit_Age = tidy(lm(1/2 * logAvgThickness + logTotalArea ~ logExposedArea, data = ., na.action = na.omit), conf.int= TRUE)) %>% unnest(cols = c(fit_Age))
-
-lm_Age <- filter(lm_Age, term == "logExposedArea")
-
-lm_Age$Age_intervalnumber <- as.numeric(factor(lm_Age$Age_interval))-1
-
-cor.test(lm_Age$estimate, lm_Age$Age_intervalnumber)

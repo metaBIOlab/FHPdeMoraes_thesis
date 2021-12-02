9c5dc202565a48e90114b371e79456585ade1915
moraesfernanda <fernandahmoraes@gmail.com>
2021-12-01 21:42:01 -0300

    update

--- Manuscript1_CorticalFolding_aging_MCI_AD/analises/reorg/analise_follow/diferenca_idade.R
+++
@@ -1,9 +0,0 @@
-# diferenca de idade
-
-dados_wide = within(dados_wide, {
-  AgeDiff = ifelse(
-    is.na(dados_wide$Age.3),
-    dados_wide$Age.2 - dados_wide$Age.1,
-    ifelse((dados_wide$Age.3 >= 0), dados_wide$Age.3 - dados_wide$Age.1)
-  )
-})
